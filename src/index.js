045885eafd2093a6bfb681ba76b0c5fa9dd3d39c
aspowerr <31863149+aspowerr@users.noreply.github.com>
2018-09-27 14:45:46 +0300

    all passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 != 0)
+  {
+    return false;
+  }
+
+  let mass = [], i = 0, j = 0;
+
+  for(i = 0; i < str.length; i++)
+  {
+      for (j = 0; j < bracketsConfig.length; j++) 
+      {
+          if(str[i] == bracketsConfig[j][0]  && 
+            bracketsConfig[j][0] == bracketsConfig[j][1] &&
+             bracketsConfig[j][0] == mass[mass.length - 1])
+             {
+              mass.pop();
+              break;
+            }
+  
+          if (str[i] == bracketsConfig[j][0])
+          {
+  
+              mass.push(str[i]);
+              break;
+          }
+          if(str[i] == bracketsConfig[j][1])
+          {
+                  if(bracketsConfig[j][0] != mass.pop()){
+  
+                      return false;
+                  }
+          }
+      }
+  }
+  
+  if(mass.pop()) 
+  { 
+    return false; 
+  }
+  return true;
 }
